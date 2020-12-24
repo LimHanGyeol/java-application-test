3ffaf063e353f73d6f8b2b7fa541e462428810a4
임한결 <dlagksruf19@naver.com>
2020-12-24 19:44:18 +0900

    [JUnit5] JUnit5 테스트 반복하기 1부

--- src/test/java/com/tommy/test/GroupTest.java
+++ src/test/java/com/tommy/test/GroupTest.java
@@ -1,8 +1,8 @@
 package com.tommy.test;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -29,4 +29,19 @@ public class GroupTest {
     void customTag() {
         assertThat(new Study(5).getLimit()).isEqualTo(5);
     }
+
+    @RepeatedTest(value = 5, name = "{displayName}, {currentRepetition}/{totalRepetitions}")
+    @DisplayName("테스트 반복")
+    void repeat(RepetitionInfo repetitionInfo) {
+        System.out.println("총 반복 횟수 : " + repetitionInfo.getTotalRepetitions());
+        System.out.println("현재 반복 횟수 : " + repetitionInfo.getCurrentRepetition());
+    }
+
+    @ParameterizedTest(name = "{index} {displayName} message = {0}")
+    @DisplayName("현재 날씨 확인")
+    @ValueSource(strings = {"날씨가", "많이", "추워지고", "있네요."})
+    void parameterizedTest(String message) {
+        System.out.println(message);
+    }
+
 }
