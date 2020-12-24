150c0deed92da6f6ee22da27eaaaa50a8a0328cf
임한결 <dlagksruf19@naver.com>
2020-12-24 21:26:18 +0900

    [JUnit5] JUnit5 테스트 순서

--- src/test/java/com/tommy/test/InstanceTest.java
+++ src/test/java/com/tommy/test/InstanceTest.java
@@ -1,13 +1,15 @@
 package com.tommy.test;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.*;
 
 /**
  * 인스턴스가 class 단위로 유지가 된다.
+ * TestInstance 와 TestMethodOrder 어노테이션은 연관관계가 없다.
+ * 독립적으로 사용 가능하다.
+ * 하지만 유즈케이스 기반으로 시나리오 테스트를 만들때 두 기능을 사용함이 유용하다고 생각한다.
  */
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class InstanceTest {
 
     private int value = 1;
@@ -19,12 +21,14 @@ public class InstanceTest {
     }
 
     @Test
+    @Order(2)
     void one() {
         System.out.println(value);
         value++;
     }
 
     @Test
+    @Order(1)
     void two() {
         System.out.println(value);
     }
